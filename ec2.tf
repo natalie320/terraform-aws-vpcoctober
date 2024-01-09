19ff705f1e93f9d925fd450244c46eba1ca0722c
Natalie <natalieshevchuk3@gmail.com>
2024-01-09 03:34:53 +0000

    fixed issue

--- ec2.tf
+++ ec2.tf
@@ -1,74 +1,34 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
-resource "aws_subnet" "main1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet1_cidr
-  availability_zone = "us-east-2a"
-  map_public_ip_on_launch = var.type
-  tags = {
-    Name = "Main1"
-  }
-}
-
-resource "aws_subnet" "main2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet2_cidr
-  availability_zone = "us-east-2b"
-  map_public_ip_on_launch = var.type
-  tags = {
-    Name = "Main2"
-  }
-}
-
-resource "aws_subnet" "main3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet3_cidr
-  availability_zone = "us-east-2c"
-  map_public_ip_on_launch = var.type
-  tags = {
-    Name = "Main3"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  tags = {
-    Name = "example"
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main1.id
-  route_table_id = aws_route_table.example.id
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.main1.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = aws_key_pair.deployer.key_name
+  user_data = file("apache.sh")
 }
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.main2.id
-  route_table_id = aws_route_table.example.id
+output ec2 {
+  value = aws_instance.web.public_ip
 }
 
-resource "aws_route_table_association" "c" {
-  subnet_id      = aws_subnet.main3.id
-  route_table_id = aws_route_table.example.id
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = file("~/.ssh/id_rsa.pub")
 }
